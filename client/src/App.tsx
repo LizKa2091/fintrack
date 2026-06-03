6805952c9ebab1f5a17a367bcd5e48d7f642a34e
lizka <lizka4231@gmail.com>
2026-06-03 23:12:23 +0300

    feat(client): add scss modules support, global vars

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
+import styles from './App.module.scss'
 import { type FC } from 'react'
 
 const App: FC = () => {
    return (
-      <div style={{ fontFamily: 'sans-serif', padding: '40px', textAlign: 'center' }}>
-         <h1>FinTrack</h1>
-         <p>Сервис для учёта личных финансов</p>
+      <div className={styles.container}>
+         <header className={styles.header}>
+            <h1>
+               FinTrack
+            </h1>
+            <p>Сервис для учёта личных финансов</p>
+         </header>
+
+         <main className={styles.dashboardMock}>
+            <h2>Добро пожаловать в панель управления</h2>
+         </main>
       </div>
    )
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   plugins: [react()],
+   css: {
+      preprocessorOptions: {
+         scss: {
+         additionalData: `
+            @use "@/styles/_variables.scss" as *;
+            @use "@/styles/_mixins.scss" as *;
+         `
+         }
+      }
+   },
+   resolve: {
+      alias: {
+         '@': '/src',
+      },
+   },
 })
