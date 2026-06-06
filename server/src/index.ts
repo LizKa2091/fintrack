499f3668bb7065353cb4de101db0751c8ca6356b
lizka <lizka4231@gmail.com>
2026-06-06 23:56:22 +0300

    feat(server): add authRouter for /api/auth route

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from '@/config/db.js'
 import { transactionsRouter } from '@/routes/transactionsRoutes.js'
+import authRouter from './routes/auth.routes.js' 
 
 dotenv.config()
 
@@ -12,11 +13,12 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/auth', authRouter)
 app.use('/api/transactions', transactionsRouter)
 
 app.get('/api/health', (req, res) => {
    res.json({ status: 'ok', message: 'Server is running smoothly' })
-})
+ })
 
 connectDB().then(() => {
    app.listen(PORT, () => {
