9728eaa307ff34438a590a67c24be22d2d112bc6
lizka <lizka4231@gmail.com>
2026-06-19 21:53:58 +0300

    feat: add github pages deploy setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
    plugins: [react()],
+   base: '/fintrack/',
    css: {
       preprocessorOptions: {
          scss: {
@@ -15,7 +17,7 @@ export default defineConfig({
    },
    resolve: {
       alias: {
-         '@': '/src',
+         '@': path.resolve(__dirname, './src'),
       },
    },
 })
